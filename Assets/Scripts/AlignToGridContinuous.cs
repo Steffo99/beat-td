3d9cf176b29f239080ebc5e85b59671194c99f69
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-04-21 11:40:20 +0200

    Add snapped to grid cursor

---
+++ Assets/Scripts/AlignToGridContinuous.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignToGridContinuous : MonoBehaviour {
+	
+	void Update () {
+        float x = Mathf.Round(transform.position.x);
+        float y = Mathf.Round(transform.position.y);
+        float z = Mathf.Round(transform.position.z);
+        transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ Assets/Scripts/PositionFromMouse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionFromMouse : MonoBehaviour {
+    
+	void Update () {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(worldPosition.x, worldPosition.y, transform.position.z);
+    }
+}
