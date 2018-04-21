80b9ac2bb19db308deab4c3500aed930e90a15a4
Stefano Pigozzi <ste.pigozzi@gmail.com>
2018-04-21 13:01:00 +0200

    Create prefab placer

---
+++ Assets/Scripts/InstantiateOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateOnClick : MonoBehaviour
+{
+    public GameObject original = null;
+
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            if (original != null)
+            {
+                Instantiate(original, transform.position, transform.rotation);
+            }
+        }
+	}
+}
